d0943a656233ca8ed1d7dfb6aa9d628dec453426
rubens23 <rubens_assis@outlook.com.br>
2022-06-17 14:15:05 -0300

    grid layout custom implementado
    implementei o grid layout custom e vou tentar adaptar ele para o que eu preciso

--- AppAcademia/app/src/main/java/com/example/circlepicture/ActivityEvolucao.java
+++ AppAcademia/app/src/main/java/com/example/circlepicture/ActivityEvolucao.java
@@ -1,297 +1,58 @@
 package com.example.circlepicture;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
-import android.app.ActionBar;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageMetadata;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.circlepicture.databinding.ActivityEvolucaoBinding;
+
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Random;
-import java.util.UUID;
+import java.util.List;
 
 public class ActivityEvolucao extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    //TODO como lidar com datas aqui? Como agrupar fotos na mesma semana, dia, mes ou ano?
-   
-
-    FloatingActionButton btn_new_photo;
-    Uri filePath;
-    Uri photoURI;
-
-    FirebaseStorage firebaseStorage;
-    StorageReference storageReference;
-
-    FirebaseUser user;
-    FirebaseAuth auth;
-    String id_user;
-
-    LinearLayout layout;
-
-    String currentPhotoPath;
-
-
+    private ActivityEvolucaoBinding binding;
+    private SimpleAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_evolucao);
+        binding = ActivityEvolucaoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        BancoController bc = new BancoController(this);
-        Cursor cursor = bc.getNumberOfImages(FirebaseAuth.getInstance().getUid());
+        //Your RecyclerView
+        binding.recycler.setHasFixedSize(true);
+        binding.recycler.setLayoutManager(new GridLayoutManager(this, 4));
 
+        //Your recyclerview adapter
+        mAdapter = new SimpleAdapter(this);
 
+        //This is the code to provide a sectioned grid
+        List<SectionedGridRecyclerViewAdapter.Section> sections = new ArrayList<SectionedGridRecyclerViewAdapter.Section>();
 
-        if(cursor.getCount()==0){
-            TextView txtSemImagens = findViewById(R.id.txtSemImagens);
-            txtSemImagens.setVisibility(View.VISIBLE);
-        }else{
-            ArrayList<String> imagesLinks = new ArrayList();
-            do {
-                if (cursor.getString(0) != null) {
-                    imagesLinks.add(cursor.getString(0));
-                }
-            }while (cursor.moveToNext()) ;
-            GridView gridView = findViewById(R.id.myGrid);
-            gridView.setAdapter(new GalleryAdapter(imagesLinks, this));
-        }
+        //Sections
+        sections.add(new SectionedGridRecyclerViewAdapter.Section(0,"Section 1"));
+        sections.add(new SectionedGridRecyclerViewAdapter.Section(5,"Section 2"));
+        sections.add(new SectionedGridRecyclerViewAdapter.Section(12,"Section 3"));
+        sections.add(new SectionedGridRecyclerViewAdapter.Section(14,"Section 4"));
+        sections.add(new SectionedGridRecyclerViewAdapter.Section(20,"Section 5"));
 
-        btn_new_photo = findViewById(R.id.btn_newImage);
-        firebaseStorage = FirebaseStorage.getInstance();
-        storageReference = firebaseStorage.getReference();
+        //add your adapter to the sectionAdapter
+        //Add your adapter to the sectionAdapter
+        SectionedGridRecyclerViewAdapter.Section[] dummy = new SectionedGridRecyclerViewAdapter.Section[sections.size()];
+        SectionedGridRecyclerViewAdapter mSectionedAdapter = new
+                SectionedGridRecyclerViewAdapter(this,R.layout.section,R.id.section_text,binding.recycler,mAdapter);
+        mSectionedAdapter.setSections(sections.toArray(dummy));
 
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
-        id_user = user.getUid();
-
-
-        btn_new_photo.setOnClickListener(v->{
-            takePicture();
-        });
-    }
-
-    private void colorrandom(ImageView imageView) {
-
-        Random random = new Random();
-        int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
-        imageView.setBackgroundColor(color);
-    }
-
-    private void addView(ImageView imageView, int width, int height) {
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, height);
-
-        params.setMargins(0, 10, 0, 10);
-        imageView.setLayoutParams(params);
-
-        layout.addView(imageView);
-    }
-
-    private void takePicture() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if(intent.resolveActivity(getPackageManager()) !=null){
-            //Create the file where the photo should go
-            File photoFile = null;
-            try{
-                photoFile = createImageFile();
-            }catch (IOException ex){
-                //error ocurred while creating the file
-            }
-            //continue only if the file was successfully created
-            if(photoFile != null){
-                photoURI = FileProvider.getUriForFile(this, "com.example.circlepicture", photoFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
-
-        //todo dar uma olhada no tutorial para salvar a imagem com alta resolução(link que ta no meu whats)
+        //Apply this adapter to the RecyclerView
+        binding.recycler.setAdapter(mSectionedAdapter);
 
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        //todo eu aparentemente gerei o arquivo com a foto em alta resolução mas eu acho que eu ainda tenho que configurar esse metodo
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            Bitmap bitmap;
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoURI);
-                String path = MediaStore.Images.Media.insertImage(this.getContentResolver(), bitmap, "img_"+ Calendar.getInstance().getTime(), null);
-                filePath = Uri.parse(path);
-                uploadImageToFirebaseStorage();
-
-            }catch (FileNotFoundException e){
-                Toast.makeText(this, "primeiro catch: "+e.getMessage(), Toast.LENGTH_LONG).show();
-            }catch (IOException e){
-                Toast.makeText(this, "segundo catch: "+e.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }
-        /*
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && data != null){
-            Bundle extras = data.getExtras();
-            Bitmap bitmapReference = (Bitmap) extras.get("data");
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            //bitmapReference.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-            String path = MediaStore.Images.Media.insertImage(this.getContentResolver(), bitmapReference, "Title", null);
-            Toast.makeText(this, "path:  "+path, Toast.LENGTH_LONG).show();
-            filePath = Uri.parse(path);
-            uploadImageToFirebaseStorage();
-
-            //aqui nós temos a captura dos dados que compõem a imagem
-        }else{
-            Toast.makeText(this, "os requests code são diferentes!!", Toast.LENGTH_LONG).show();
-        }
-
-         */
-
-
-
-
-
-
-    }
-
-    private File createImageFile() throws IOException {
-        //create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_"+timeStamp+"_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-
-        // Save a file: path for use with action_view intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void uploadImageToFirebaseStorage() {
-        String nomeImagem = UUID.randomUUID().toString();
-        StorageReference ref = storageReference.child("images/"+ nomeImagem);
-        ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(ActivityEvolucao.this, "imagem salva com sucesso no firebase", Toast.LENGTH_SHORT).show();
-                BancoController bc = new BancoController(ActivityEvolucao.this);
-                String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-                //aqui eu posso usar o banco de dados para pegar o link ou os links das imagens e usar esse link para compor o gridView
-                ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        bc.insertNewPhoto(id_user, date, nomeImagem, uri.toString());//TODO testar tudo isso///ta com algum erro
-                        //testando os metodos do storagemetadata da referencia do storage ao tirar uma foto nova
-                       ref.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
-                           @Override
-                           public void onSuccess(StorageMetadata storageMetadata) {
-                               long creationTime = storageMetadata.getCreationTimeMillis();
-                               Calendar cal = Calendar.getInstance();
-                               String ano = Integer.toString(cal.get(Calendar.YEAR));
-                               String mes = Integer.toString(cal.get(Calendar.MONTH));
-                               String dia = Integer.toString(cal.get(Calendar.DAY_OF_MONTH));
-                               switch(mes){
-                                   case "0":
-                                       //janeiro
-                                       break;
-                                   case "1":
-                                       //fevereiro
-                                       break;
-                                   case "2":
-                                       //março
-                                       break;
-                                   case "3":
-                                       //abril
-                                       break;
-                                   case "4":
-                                       //maio
-                                       break;
-                                   case "5":
-                                       //junho
-                                       break;
-                                   case "6":
-                                       //julho
-                                       break;
-                                   case "7":
-                                       //agosto
-                                       break;
-                                   case "8":
-                                       //setembro
-                                       break;
-                                   case "9":
-                                       //outubro
-                                       break;
-                                   case "10":
-                                       //novembro
-                                       break;
-                                   case "11":
-                                       //dezembro
-                                       break;
-                               }
-
-                           }
-                       })
-                               .addOnFailureListener(new OnFailureListener() {
-                                   @Override
-                                   public void onFailure(@NonNull Exception e) {
-                                        //não conseguiu pegar os metadados
-                                   }
-                               });
-
-                    }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(ActivityEvolucao.this, "erro ao colocar a imagem no storage: "+e.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    public void downloadImage(){
-        //esse método talvez seja importante
-    }
 }
 
 //link sectiloned grid:
---
+++ AppAcademia/app/src/main/java/com/example/circlepicture/SimpleAdapter.java
@@ -0,0 +1,67 @@
+package com.example.circlepicture;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.SimpleViewHolder> {
+    private static final int COUNT = 100;
+
+    private final Context mContext;
+    private final List<Integer> mItems;
+    private int mCurrentItemId = 0;
+
+    public SimpleAdapter(Context context){
+        mContext = context;
+        mItems = new ArrayList<Integer>(COUNT);
+        for(int i = 0; i < COUNT; i++){
+            addItem(i);
+        }
+    }
+
+    @NonNull
+    @Override
+    public SimpleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(mContext).inflate(R.layout.item, parent, false);
+        return new SimpleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SimpleViewHolder holder, int position) {
+        holder.title.setText(mItems.get(position).toString());
+
+    }
+
+    public void addItem(int position){
+        final int id = mCurrentItemId++;
+        mItems.add(position, id);
+        notifyItemInserted(position);
+    }
+
+    public void removeItem(int position){
+        mItems.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder{
+        public final TextView title;
+
+        public SimpleViewHolder(View view){
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+        }
+    }
+}
